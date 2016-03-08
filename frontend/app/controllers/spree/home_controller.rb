b19b53cb7140e40577263d78daf2014a9d37e6d2
Damian Legawiec <damian@sparksolutions.co>
2016-03-08 15:29:18 +0100

    full page caching: home page

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -1,5 +1,6 @@
 module Spree
   class HomeController < Spree::StoreController
+    include Spree::ProductsHelper
     helper 'spree/products'
     respond_to :html
 
@@ -7,6 +8,17 @@ module Spree
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions)
       @taxonomies = Spree::Taxonomy.includes(root: :children)
+      fresh_when etag: home_etag, last_modified: home_last_modified, public: true
+    end
+
+    protected
+
+    def home_etag
+      'home/' + cache_key_for_products + '/' + @taxonomies.maximum(:updated_at).to_s
+    end
+
+    def home_last_modified
+      [@taxonomies.maximum(:updated_at), @products.maximum(:updated_at)].compact.max.try(:utc) || Time.now.utc
     end
   end
 end
--- frontend/spec/controllers/spree/home_controller_spec.rb
+++ frontend/spec/controllers/spree/home_controller_spec.rb
@@ -24,4 +24,77 @@ describe Spree::HomeController, :type => :controller do
       end
     end
   end
+
+  context 'http caching' do
+    let!(:product) { create(:product) }
+    let!(:taxonomy) { create(:taxonomy) }
+
+    context 'on the first request' do
+      it 'returns a 200' do
+        spree_get :index
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'on a subsequent request' do
+      before do
+        spree_get :index
+        expect(response.status).to eq(200)
+        expect(response.headers['ETag']).to be_present
+        expect(response.headers['Last-Modified']).to be_present
+        @etag = response.headers['ETag']
+        @last_modified = response.headers['Last-Modified']
+      end
+
+      context "if it is not stale" do
+        before do
+          request.env['HTTP_IF_NONE_MATCH'] = @etag
+          request.env['HTTP_IF_MODIFIED_SINCE'] = @last_modified
+        end
+
+        it 'returns a 304' do
+          spree_get :index
+          expect(response.status).to eq(304)
+        end
+      end
+
+      context 'if product has been updated' do
+        before do
+          sleep 1 # To ensure the product.updated_at has a delta of at least 1 sec
+          product.touch
+          request.env['HTTP_IF_NONE_MATCH'] = @etag
+          request.env['HTTP_IF_MODIFIED_SINCE'] = @last_modified
+        end
+
+        it 'returns a 200' do
+          spree_get :index
+          expect(response.status).to eq(200)
+        end
+
+        it 'returns product updated_at as last modified' do
+          spree_get :index
+          expect(Time.parse(response.headers['Last-Modified']).utc.to_i).to eq(product.updated_at.utc.to_i)
+        end
+      end
+
+      context 'if taxonomy has been updated' do
+        before do
+          sleep 1 # To ensure the product.updated_at has a delta of at least 1 sec
+          taxonomy.touch
+          request.env['HTTP_IF_NONE_MATCH'] = @etag
+          request.env['HTTP_IF_MODIFIED_SINCE'] = @last_modified
+        end
+
+        it 'returns a 200' do
+          spree_get :index
+          expect(response.status).to eq(200)
+        end
+
+        it 'returns product updated_at as last modified' do
+          spree_get :index
+          expect(Time.parse(response.headers['Last-Modified']).utc.to_i).to eq(taxonomy.updated_at.utc.to_i)
+        end
+      end
+    end
+  end
 end
